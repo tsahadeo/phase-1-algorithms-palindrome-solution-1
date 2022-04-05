41b564828513d24547670afc8294ee9058c862f4
Travis <tsahadeo@mac.com>
2022-04-05 10:54:09 -0400

    commit tree aa591cfc6ab75cbcaddbe22e4bf6961a046cbe63

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
